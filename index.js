94447db8e6bd223dd3fdecf1a3563e442b61bb53
Neebian <vrabtam@gmail.com>
2016-02-15 14:36:22 +0100

    Add connection to index

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
 
 var config = require('./server/config.js');
 var createServer = require('./server/server.js');
+var Connection = require('./server/connection.js');
+var connection = new Connection();
 
-var app = createServer();
+var app = createServer(connection);
 
 app.listen(config.DEFAULT_PORT, function() {
 	console.log('Listening on port ' + config.DEFAULT_PORT + '...');
