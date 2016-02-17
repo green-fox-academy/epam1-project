62cedaad88befbec4faeadb5f7d4c4a6d8e91342
Norbert Pozsonyi <norbert.pozsonyi@gmail.com>
2016-02-17 14:38:29 +0100

    create log factory, add InnerDate, loggerFunction, loggingLevel dependencies

--- server/log.js
+++ server/log.js
@@ -2,37 +2,33 @@
 
 var config = require('./config.js');
 
-function Log() {
-  var _this = this;
-  this.levels = ['debug', 'info', 'warn', 'error'];
-  this.loggingLevel = config.DEFAULT_LOGGING_LEVEL;
+function createLog(loggingLevel, loggerFunction, InnerDate) {
+  loggingLevel = loggingLevel || config.DEFAULT_LOGGING_LEVEL;
+  loggerFunction = loggerFunction || console.log;
+  InnerDate = InnerDate || Date;
+  var levels = ['debug', 'info', 'warn', 'error'];
 
-  this.logRequest = function (req, res, next) {
-    _this.createLogEntry({
-      level: 'info',
-      name: 'NEW REQUEST',
-      method: req.method,
-      url: req.originalUrl,
-    });
-    next();
-  };
-
-  this.message = function (level, message) {
-    _this.createLogEntry({
+  function message(level, message) {
+    createLogEntry({
       level: level,
+      date: new InnerDate(),
       message: message,
     });
-  };
+  }
 
-  this.createLogEntry = function (messages) {
-    if (_this.isValidLevel(messages.level)) {
-      console.log(JSON.stringify(messages));
+  function createLogEntry(messages) {
+    if (isValidLevel(messages.level)) {
+      loggerFunction(JSON.stringify(messages));
     }
-  };
+  }
 
-  this.isValidLevel = function (level) {
-    return _this.levels.indexOf(level) >= _this.levels.indexOf(_this.loggingLevel);
+  function isValidLevel(level) {
+    return levels.indexOf(level) >= levels.indexOf(loggingLevel);
+  }
+
+  return {
+    message: message,
   };
 }
 
-module.exports = Log;
+module.exports = createLog;
--- server/server.js
+++ server/server.js
@@ -6,15 +6,18 @@ var path = require('path');
 
 var HeartQuery = require('./heartbeat/heartbeat-query.js');
 var HeartBeat = require('./heartbeat/heartbeat.js');
-var Logger = require('./log.js');
+var logger = require('./log.js')();
 
 function createServer(connection) {
   var queries = new HeartQuery(connection);
   var heartController = new HeartBeat(queries);
-  var logger = new Logger();
 
   var app = express();
-  app.use(logger.logRequest);
+  app.use(function (req, res, next) {
+    logger.message('info', `NEW REQUEST, method=${req.method}, url=${req.originalUrl}`);
+    next();
+  });
+
   app.use(bodyParser.json());
 
   var route = path.join(__dirname, '..', 'public');
