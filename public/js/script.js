3fa8e39e361b963e2b6031947e0a15b8231c8261
Vrandi <vrabely.andrea@gmail.com>
2016-02-18 16:32:08 +0100

    Add factory, controller and new state to myapp

--- public/js/script.js
+++ public/js/script.js
@@ -9,7 +9,6 @@ myapp.config(function ($stateProvider, $urlRouterProvider) {
       url: '/',
       templateUrl: '../templates/frontpage.html',
     })
-
     .state('register', {
       url: '/register',
       templateUrl: '../templates/register.html',
@@ -17,5 +16,37 @@ myapp.config(function ($stateProvider, $urlRouterProvider) {
     .state('login', {
       url: '/login',
       templateUrl: '../templates/login.html',
+    })
+    .state('home', {
+      url: '/home',
+      templateUrl: '../templates/home.html',
     });
+});
+
+myapp.factory('users', function ($http, $state) {
+  var url = '/api/register';
+
+  function addNewUser(newUser) {
+    $http.post(url, newUser).then(function (response) {
+      $state.go('home');
+      console.log(response);
+    }, function (error) {
+
+      console.log(error);
+    });
+  }
+
+  return {
+    addNewUser: addNewUser,
+  };
+});
+
+myapp.controller('SubmitCtrl', function ($scope, users) {
+  $scope.addUser = function () {
+    var user = {
+      email: $scope.email,
+      password: $scope.password,
+    };
+    users.addNewUser(user);
+  };
 });
