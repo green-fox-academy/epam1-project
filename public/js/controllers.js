a4fa50afd2677fc6d97cf9197ae7d714e81c1c89
Norbert Pozsonyi <norbert.pozsonyi@gmail.com>
2016-02-19 15:02:06 +0100

    fix indentation

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,16 +1,17 @@
 'use strict';
 
-angular.module('myapp').controller('SubmitCtrl', function ($scope, users) {
-  $scope.addUser = function () {
-    var user = {
-      email: $scope.email,
-      password: $scope.password,
-    };
+angular.module('myapp')
+  .controller('SubmitCtrl', function ($scope, users) {
+    $scope.addUser = function () {
+      var user = {
+        email: $scope.email,
+        password: $scope.password,
+      };
 
-    var errorHandler = function (error) {
-      $scope.Error = error.statusText;
-    };
+      var errorHandler = function (error) {
+        $scope.Error = error.statusText;
+      };
 
-    users.addNewUser(user, errorHandler);
-  };
-});
+      users.addNewUser(user, errorHandler);
+    };
+  });
