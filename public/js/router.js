e8cccbd19743c3c66f522ebc9c6a753eddcd6862
Norbert Pozsonyi <norbert.pozsonyi@gmail.com>
2016-02-22 15:02:36 +0100

    change index.html title on state change

--- public/js/router.js
+++ public/js/router.js
@@ -7,28 +7,30 @@ angular.module('myapp', ['ui.router'])
       .state('frontpage', {
         url: '/',
         templateUrl: './templates/frontpage.html',
+        pageTitle: 'Epam@Interviewer',
       })
       .state('register', {
         url: '/register',
         templateUrl: './templates/register.html',
+        pageTitle: 'Register',
       })
       .state('login', {
         url: '/login',
         templateUrl: './templates/login.html',
+        pageTitle: 'Login',
       })
       .state('home', {
         url: '/home',
         templateUrl: './templates/home.html',
+        pageTitle: 'Home',
       }
       );
   })
   .run(function ($rootScope, $http) {
     $rootScope.$on('$stateChangeStart',
       function (event, toState) {
+        window.document.title = toState.pageTitle;
         var logMessage = { level: 'info', toState: toState.url };
-        $http.post('/api/log', logMessage)
-          .then(function (response) {
-            console.log(response.data);
-          });
+        $http.post('/api/log', logMessage);
       });
   });
