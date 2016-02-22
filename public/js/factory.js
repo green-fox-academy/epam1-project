95bfdf8cb9423b2391ee2aa0fcc689f38e6e3b6a
Vrandi <vrabely.andrea@gmail.com>
2016-02-22 12:12:23 +0100

    Add loginUser function

--- public/js/factory.js
+++ public/js/factory.js
@@ -1,19 +1,29 @@
 'use strict';
 
-angular.module('myapp').factory('users', function ($http) {
-  var url = '/api/register';
+angular.module('myapp')
+  .factory('users', function ($http) {
 
-  function addNewUser(newUser, handleResponse) {
-    $http.post(url, newUser).then(function (response) {
-      handleResponse(response);
+    function addNewUser(newUser, handleResponse) {
+      $http.post('/api/register', newUser).then(function (response) {
+        handleResponse(response);
 
-    }, function (error) {
+      }, function (error) {
 
-      handleResponse(error);
-    });
-  }
+        handleResponse(error);
+      });
+    }
 
-  return {
-    addNewUser: addNewUser,
-  };
-});
+    function loginUser(user, handleResponse) {
+      $http.post('/api/login', user).then(function (response) {
+        handleResponse(response);
+      }, function (error) {
+
+        handleResponse(error);
+      });
+    }
+
+    return {
+      addNewUser: addNewUser,
+      loginUser: loginUser,
+    };
+  });
