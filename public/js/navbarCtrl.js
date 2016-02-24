011fa5cbd29502d3851042de8735e2e3446187b9
Norbert Pozsonyi <norbert.pozsonyi@gmail.com>
2016-02-24 16:51:09 +0100

    implement logout when 'logout' button clicked

--- public/js/navbarCtrl.js
+++ public/js/navbarCtrl.js
@@ -3,4 +3,16 @@
 angular.module('myapp')
   .controller('NavbarCtrl', function ($scope, $state, user) {
     $scope.user = user;
+
+    $scope.logoutUser = function () {
+      user.logoutUser(whenLogout);
+    };
+
+    function whenLogout(error) {
+      if (error) {
+        window.alert(error.data);
+      } else {
+        $state.go('frontpage');
+      }
+    }
   });
